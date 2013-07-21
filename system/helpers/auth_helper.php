93b8def1c2e302d7f56ad33af4338820769c703e
Cristian Drincu <cristidrincu@gmail.com>
2013-07-21 13:29:04 +0300

    Created the auth_helper that contains the checkSession method - available for all controllers, avoided DRY principle.

---
+++ system/helpers/auth_helper.php
@@ -0,0 +1,14 @@
+<?php
+
+function checkSessionData($page_type, $data){
+    $ci=& get_instance();
+    $ci->load->library('session');
+    if($ci->session->userdata('logged_in')){
+        $session_data = $ci->session->userdata('logged_in');
+        $data['username'] = $session_data['username'];
+        $ci->load->view($page_type, $data);
+    }
+    else{
+        redirect('index.php/login', 'refresh');
+    }
+}
